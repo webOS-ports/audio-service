9b01f55651b02424a47f9d2c0ecb04c171f66e90
Isaac Garzon <isagar2004@gmail.com>
2015-01-19 00:19:44 +0200

    Fixed a problem with volumeDown after calling setVolume with value < VOLUME_STEP
    Signed-off-by: Isaac Garzon <isagar2004@gmail.com>

--- src/audio_service.c
+++ src/audio_service.c
@@ -373,11 +373,12 @@ static bool volume_up_cb(LSHandle *handle, LSMessage *message, void *user_data)
 		return true;
 	}
 
-	normalized_volume = (service->volume / VOLUME_STEP) * VOLUME_STEP % 100;
-	if (normalized_volume >= 88) /* because VOLUME_STEP is 11, this adjustment is needed to get from 88 to 100 */
-		++normalized_volume;
-	if (normalized_volume == 100)
+	normalized_volume = (service->volume / VOLUME_STEP) * VOLUME_STEP;
+	if (normalized_volume >= 99)
 		goto done;
+	else if (normalized_volume >= 88) /* because VOLUME_STEP is 11, this adjustment is needed to get from 88 to 100 */
+		++normalized_volume;
+
 
 	req = luna_service_req_data_new(handle, message);
 	req->user_data = service;
@@ -403,8 +404,10 @@ static bool volume_down_cb(LSHandle *handle, LSMessage *message, void *user_data
 		return true;
 	}
 
-	normalized_volume = (service->volume / VOLUME_STEP) * VOLUME_STEP % 100;
-	if (normalized_volume == 0)
+	normalized_volume = ((service->volume + VOLUME_STEP - 1) / VOLUME_STEP) * VOLUME_STEP;
+	if (normalized_volume >= 100) /* If service->volume is 100, we'd be at 110. Adjust */
+		normalized_volume = 99;
+	else if (normalized_volume == 0)
 		goto done;
 
 	req = luna_service_req_data_new(handle, message);
