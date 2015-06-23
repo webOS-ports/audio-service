ab127a3be8d0e8adae7ec79010694861cf74de7e
Simon Busch <morphis@gravedo.de>
2015-06-23 18:39:52 +0200

    Adjust copyright year
    Signed-off-by: Simon Busch <morphis@gravedo.de>

--- src/audioservice.cpp
+++ src/audioservice.cpp
@@ -1,6 +1,6 @@
 /* @@@LICENSE
 *
-* Copyright (c) 2013 Simon Busch <morphis@gravedo.de>
+* Copyright (c) 2013-2015 Simon Busch <morphis@gravedo.de>
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
